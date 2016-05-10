d4f475a10751c58351e1aa04ba79a7e93bf38f5c
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-05-10 14:38:07 +0100

    Add comms and handles clients.

---
+++ Imap/CommsClient.fs
@@ -0,0 +1,21 @@
+﻿module CommsClient
+open System
+open System.Net
+open System.Net.Http
+open System.Configuration
+open Newtonsoft.Json
+
+let url = 
+    let configUrl = ConfigurationManager.AppSettings.Item("CommsUrl")
+    if String.IsNullOrWhiteSpace configUrl then
+        failwith "Url for comms service is missing from configuration. Add url to procede."
+    else 
+        configUrl
+
+let getKnownExternalIds email = 
+    use client = new HttpClient()
+    let idsUrl = sprintf "%s/ExternalIds/?handle=%s" url email
+    let result = client.GetAsync(idsUrl).Result
+    let json = result.Content.ReadAsStringAsync().Result
+    let ids = JsonConvert.DeserializeObject<String []>(json)
+    ids 
